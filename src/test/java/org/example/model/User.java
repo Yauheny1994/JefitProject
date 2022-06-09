95baa44b89461dc1b539fe68c362929c2fa7a69b
Evgeni Makarchuk <makarchuk_evgen@inbox.ru>
2022-06-09 16:11:11 +0300

    fix mistake

--- src/test/java/org/example/model/User.java
+++ src/test/java/org/example/model/User.java
@@ -1,16 +1,12 @@
 package org.example.model;
 
+import lombok.Getter;
+
+@Getter
 public class User {
 
     private String userName = "makarik_ma@mail.ru";
 
     private String password = "Makarik_ma1@mail.ru";
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
