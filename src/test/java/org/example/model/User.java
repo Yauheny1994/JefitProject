b996ea0dd8b312ab3c29e1b611f889f0dded6447
Evgeni Makarchuk <makarchuk_evgen@inbox.ru>
2022-06-13 00:06:45 +0300

    finish version

--- src/test/java/org/example/model/User.java
+++ src/test/java/org/example/model/User.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 @Getter
 public class User {
 
-//    private final String userName = "makarik_ma@mail.ru";
-//
-//    private final String password = "Makarik_ma1@mail.ru";
-    private String userName = System.getProperty("email");
-    private String password =System.getProperty("password");
+    private final String userName = "makarik_ma@mail.ru";
+
+    private final String password = "Makarik_ma1@mail.ru";
+//    private String userName = System.getProperty("email");
+//    private String password =System.getProperty("password");
 }
