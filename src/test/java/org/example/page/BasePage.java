4eacfe247a72f5162b80344830458e900cd271c6
Evgeni Makarchuk <makarchuk_evgen@inbox.ru>
2022-06-13 17:49:05 +0300

    finish version

--- src/test/java/org/example/page/BasePage.java
+++ src/test/java/org/example/page/BasePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
+
     protected WebDriver driver = DriverSingleton.getInstance().getDriver();
 
      BasePage() {
